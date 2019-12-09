37cfcc077636a9412a81e08e8c02df5086d60129
crashantrax <crashantrax0@gmail.com>
2019-12-10 02:41:50 +0800

    update

--- lib/content/css/cssreferences.dart
+++ lib/content/css/cssreferences.dart
@@ -1,8 +1,6 @@
-
 import 'package:onlinetutorials/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 
 class Cssreferencesnav extends StatelessWidget {
   @override
@@ -24,165 +22,45 @@ class Cssreferences extends StatelessWidget {
   //     {Key key, this.idUser, this.firstname, this.lastname, this.username})
   //     : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("CSS REFERENCES"),
-        ),
-        
-        body: ListView(children: <Widget>[
+      appBar: new AppBar(
+        title: new Text("CSS REFERENCES"),
+      ),
+      body: ListView(
+        children: <Widget>[
           new Container(
-              alignment: Alignment.topRight,
-              child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+            alignment: Alignment.topRight,
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   mainAxisSize: MainAxisSize.max,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    
-                    
-
-                    // new Row(
-                    //     mainAxisAlignment: MainAxisAlignment.center,
-                    //     mainAxisSize: MainAxisSize.max,
-                    //     crossAxisAlignment: CrossAxisAlignment.center,
-                    //     children: <Widget>[
-                    //       new Padding(
-                    //         child: new Text(
-                    //           "SQL",
-                    //           style: new TextStyle(
-                    //               fontSize: 60.0,
-                    //               color: Colors.black,
-                    //               fontWeight: FontWeight.w700,
-                    //               fontFamily: "Ewert"),
-                    //         ),
-                    //         padding:
-                    //             const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-                    //       ),
-                    //     ]
-                    //     ),
-                    // new Row(
-                    //     mainAxisAlignment: MainAxisAlignment.center,
-                    //     mainAxisSize: MainAxisSize.max,
-                    //     crossAxisAlignment: CrossAxisAlignment.center,
-                    //     children: <Widget>[
-                    //       new Padding(
-                    //         child: new Text(
-                    //           "The language for accessing databases",
-                    //           maxLines: 2,
-                    //           style: new TextStyle(
-                    //               fontSize: 15.0,
-                    //               color: Colors.black,
-                    //               // fontWeight: FontWeight.w700,
-                    //               fontFamily: "Ewert"),
-                    //         ),
-                    //         padding:
-                    //             const EdgeInsets.fromLTRB(30.0, 12.0, 10.0, 12.0),
-                    //       ),
-                    //     ]
-                    //     ),
-                    // new Row(
-                    //     mainAxisAlignment: MainAxisAlignment.center,
-                    //     mainAxisSize: MainAxisSize.max,
-                    //     crossAxisAlignment: CrossAxisAlignment.center,
-                    //     children: <Widget>[
-
-                    //       Padding(
-                    //           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    //           child: new FlatButton(
-                    //             color: Colors.lightBlue,
-                    //             textColor: Colors.white,
-                    //             disabledColor: Colors.grey,
-                    //             disabledTextColor: Colors.black,
-                    //             splashColor: Colors.grey,
-                    //             onPressed: () {
-                    //               // Navigator.push(
-                    //               //   context,
-                    //               //   MaterialPageRoute(
-                    //               //       builder: (context) => LatestUpload()),
-                    //               // );
-                    //               /*...*/
-                    //             },
-                    //             child: Text(
-                    //               "LEARN SQL",
-                    //               style: TextStyle(
-                    //                   fontSize: 15.0, fontWeight: FontWeight.w600),
-                    //             ),
-                    //           )),
-                    //       Padding(
-                    //           padding: const EdgeInsets.only(left: 10.0),
-                    //           child: new FlatButton(
-                    //             color: Colors.lightBlue,
-                    //             textColor: Colors.white,
-                    //             disabledColor: Colors.grey,
-                    //             disabledTextColor: Colors.black,
-                    //             splashColor: Colors.grey,
-                    //             onPressed: () {
-                    //               // Navigator.push(
-                    //               //   context,
-                    //               //   MaterialPageRoute(
-                    //               //       builder: (context) => LatestUpload()),
-                    //               // );
-                    //               /*...*/
-                    //             },
-                    //             child: Text(
-                    //               "SQL REFERENCES",
-                    //               style: TextStyle(
-                    //                   fontSize: 15.0, fontWeight: FontWeight.w600),
-                    //             ),
-                    //           )),
-                    //     ]),
-                  ])),
-        ])
-
-        // new Container(
-        //   child: new Center(
-        //     child: new Column(
-        //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //       children: <Widget>[
-        //         // MaterialButton(
-        //         //   color: Theme.of(context).accentColor,
-        //         //   minWidth: 170.0,
-        //         //   onPressed: () {
-        //         //     var route = new MaterialPageRoute(
-        //         //       builder: (BuildContext context) =>
-        //         //           new Profile(idUser: this.idUser),
-        //         //     );
-        //         //     Navigator.of(context).push(route);
-        //         //   },
-        //         //   child: Text("Profile"),
-        //         // ),
-        //         // MaterialButton(
-        //         //   minWidth: 170.0,
-        //         //   color: Theme.of(context).accentColor,
-        //         //   onPressed: () {
-        //         //     var route = new MaterialPageRoute(
-        //         //       builder: (BuildContext context) => new AllUsers(),
-        //         //     );
-        //         //     Navigator.of(context).push(route);
-        //         //   },
-        //         //   child: Text("All Users"),
-        //         // ),
-        //         // MaterialButton(
-        //         //   minWidth: 170.0,
-        //         //   color: Theme.of(context).accentColor,
-        //         //   onPressed: () {
-        //         //     _launchURL();
-        //         //   },
-        //         //   child: Text("Call Customer Service"),
-        //         // ),
-        //         // MaterialButton(
-        //         //   minWidth: 170.0,
-        //         //   color: Theme.of(context).accentColor,
-        //         //   onPressed: () {},
-        //         //   child: Text("Econstat"),
-        //         // ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
-        );
+                    new Padding(
+                      child: new Text(
+                        "CSS References",
+                        style: new TextStyle(
+                            fontSize: 40.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Ewert"),
+                      ),
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
